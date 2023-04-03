9b962594c99d593371020a775137e288fd3541e8
enoqueJonas <enoque.macanda@gmail.com>
2023-04-03 22:02:30 +0200

    ADD models

---
+++ spec/factories/recipes.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :recipe do
+    name { "MyString" }
+    preparation_time { "9.99" }
+    cooking_time { "9.99" }
+    description { "MyString" }
+    public { false }
+    user { nil }
+  end
+end
