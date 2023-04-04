f5603ebbf72e7b84bcdfc6fd8c68eede972b3574
enoqueJonas <enoque.macanda@gmail.com>
2023-04-04 19:43:10 +0200

    ADD form to insert products

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -8,10 +8,23 @@ class FoodsController < ApplicationController
     end
 
     def new
-
+        @food = Food.new
     end
 
     def create
+        @food = Food.new params_provider
+        @food.user = current_user
 
+        if @food.save
+            flash[:notice] = 'Food created succesfully'
+            redirect_to foods_path
+        else
+            flash[:alert] = 'Error'
+            render :new
+        end
+    end
+
+    def params_provider
+        params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
     end
 end
