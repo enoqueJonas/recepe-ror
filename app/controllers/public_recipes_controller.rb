9b24e9c684aac7ecc9b7bbc22b04b538836450ef
enoqueJonas <enoque.macanda@gmail.com>
2023-04-05 23:21:00 +0200

    IMPROVE public_recipes to display starting from newest

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,6 +1,6 @@
 class  PublicRecipesController < ApplicationController
     def index
-        @recipes = Recipe.where(public: true)
+        @recipes = Recipe.where(public: true).order(created_at: :desc)
         @users = User.all
         @recipe_foods = RecipeFood.all
     end
