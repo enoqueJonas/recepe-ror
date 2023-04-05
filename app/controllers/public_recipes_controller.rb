57c28cf7d37fdf99e88a371a54447e463a320288
enoqueJonas <enoque.macanda@gmail.com>
2023-04-05 22:23:49 +0200

    UPDATE app

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,6 +1,7 @@
 class  PublicRecipesController < ApplicationController
     def index
         @recipes = Recipe.where(public: true)
+        @users = User.all
     end
 
     def show
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,8 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
+
+  def user_name
+    user.name
+  end
 end
