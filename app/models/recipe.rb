bb4cbe34dc94016cf97ac8ba4aa156640a39c397
enoqueJonas <enoque.macanda@gmail.com>
2023-04-05 23:40:57 +0200

    FIX linters

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,15 +4,15 @@ class Recipe < ApplicationRecord
   def user_name
     user.name
   end
-  
+
   has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods
 
   def total_price
-    total = 0 
+    total = 0
     recipe_foods.each do |rf|
       food = Food.find(rf.food_id)
-      total = total + (rf.quantity * food.price)
+      total += (rf.quantity * food.price)
     end
     total
   end
@@ -20,7 +20,7 @@ class Recipe < ApplicationRecord
   def total_quantity
     qt = 0
     recipe_foods.each do |rf|
-      qt += rf.quantity if rf.recipe_id == self.id
+      qt += rf.quantity if rf.recipe_id == id
     end
     qt
   end
